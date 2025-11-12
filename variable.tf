87ec33ed6e3ba97fe82c217f2558b9ee1a0dff0d
shiva <narrashivakumarreddy@gmail.com>
2025-11-12 10:09:42 +0000

    changes in main branch

--- variable.tf
+++ variable.tf
@@ -1,4 +1,20 @@
+variable "rg_name" {
+  type    = string
+  default = "tf-github-actions-rg"
+}
 variable "location" {
   type    = string
-  default = "westeurope"
+  default = "West Europe" # change to your region
+}
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+variable "ssh_public_key_path" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
 }
