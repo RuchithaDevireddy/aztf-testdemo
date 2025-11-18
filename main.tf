f67e288a1f52ef3eb5517bfa7963d13f44e9e08f
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-11-18 16:52:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -150,27 +150,4 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   depends_on = [azurerm_network_interface.nic2]
 }
 
-############################################
-# 7. VNet Peering (after both VNets are ready)
-############################################
-resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
-  name                      = "vnet1-to-vnet2"
-  resource_group_name       = azurerm_resource_group.rg.name
-  virtual_network_name      = azurerm_virtual_network.vnet1.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
-  allow_forwarded_traffic   = true
-  allow_virtual_network_access = true
 
-  depends_on = [azurerm_virtual_network.vnet2]
-}
-
-resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
-  name                      = "vnet2-to-vnet1"
-  resource_group_name       = azurerm_resource_group.rg.name
-  virtual_network_name      = azurerm_virtual_network.vnet2.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
-  allow_forwarded_traffic   = true
-  allow_virtual_network_access = true
-
-  depends_on = [azurerm_virtual_network.vnet1]
-}
