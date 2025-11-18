7608339816bb659a3e86e6ab3c08be630a7c5e84
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-11-18 16:32:57 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -22,3 +22,7 @@ variable "ssh_public_key_path" {
   type    = string
   default = "~/.ssh/id_rsa.pub"
 }
+variable "environment" {
+  description = "Deployment environment name"
+  type        = string
+}
