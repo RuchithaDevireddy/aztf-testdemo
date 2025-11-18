c068dd6ef9dbd52551f7b3e334df69924a540332
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-11-18 16:29:17 +0000

    Update variable defaults and remove environment variable
    Updated default values for resource group and location variables. Removed the environment variable.

--- variable.tf
+++ variable.tf
@@ -1,26 +1,24 @@
 variable "rg_name" {
   type    = string
-  default = "tf-github-actions-rg"
+  default = "demo-multi-vm-rg"
 }
+
 variable "location" {
   type    = string
-  default = "West Europe" # change to your region
+  default = "East US"
 }
+
 variable "vm_size" {
   type    = string
   default = "Standard_B1s"
 }
+
 variable "admin_username" {
   type    = string
   default = "azureuser"
 }
+
 variable "ssh_public_key_path" {
   type    = string
   default = "~/.ssh/id_rsa.pub"
 }
-variable "environment" {
-  description = "Environment name (dev or test)"
-  type        = string
-  default     = "dev"
-}
-
