c1ed6205583274bf93f2441083cd3da7ff1192a6
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-04 16:14:55 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,6 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.rg.name
-  description = "Name of the resource group."
-}
-
 output "function_app_name" {
-  value       = azurerm_windows_function_app.func.name
-  description = "Name of the Azure Function App."
+  value = azurerm_windows_function_app.func.name
 }
-
-output "storage_account_name" {
-  value       = azurerm_storage_account.sa.name
-  description = "Storage account name."
-}
-
-output "app_insights_connection_string" {
-  value       = azurerm_application_insights.appi.connection_string
-  sensitive   = true
-  description = "App Insights connection string."
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
 }
