1beff83bab0fa45041ab084323c0c2b2e462ad88
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-04 16:17:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">=1.3.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 # ---------------------------
 # Resource Group
