1958ac665684b99f9c1826f67a1be0d4342fcd84
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-04 16:09:53 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,20 @@
-output "vm1_public_ip" {
-  value = azurerm_public_ip.pip1.ip_address
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "Name of the resource group."
 }
 
-output "vm2_public_ip" {
-  value = azurerm_public_ip.pip2.ip_address
+output "function_app_name" {
+  value       = azurerm_windows_function_app.func.name
+  description = "Name of the Azure Function App."
+}
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.sa.name
+  description = "Storage account name."
+}
+
+output "app_insights_connection_string" {
+  value       = azurerm_application_insights.appi.connection_string
+  sensitive   = true
+  description = "App Insights connection string."
 }
