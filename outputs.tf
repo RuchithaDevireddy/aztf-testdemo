b565bf1ba9c9633f223f0087ffdbd77e542e1389
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-04 16:43:50 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,3 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "function_app_name" {
-  value = azurerm_linux_function_app.func.name
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
-
-output "app_insights_connection_string" {
-  value     = azurerm_application_insights.appi.connection_string
-  sensitive = true
+output "vm_public_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
 }
