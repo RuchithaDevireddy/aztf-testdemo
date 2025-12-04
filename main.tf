7669f27826771b66bf2c857594bdf006198b2d9d
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-04 16:14:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,9 +73,4 @@ resource "azurerm_windows_function_app" "func" {
   }
 }
 
-output "function_app_name" {
-  value = azurerm_windows_function_app.func.name
-}
-output "resource_group" {
-  value = azurerm_resource_group.rg.name
-}
+
