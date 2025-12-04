a62940e6906a82b1f71847479212f56d2eab9144
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-04 16:33:30 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,16 @@
-output "function_app_name" {
-  value = azurerm_windows_function_app.func.name
-}
-output "resource_group" {
+output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+}
+
+output "function_app_name" {
+  value = azurerm_linux_function_app.func.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "app_insights_connection_string" {
+  value     = azurerm_application_insights.appi.connection_string
+  sensitive = true
 }
