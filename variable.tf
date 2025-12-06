cc2b5d0eb8e551a21585f89a477a2d6388ad83eb
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-06 14:28:08 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,97 +1,34 @@
-# Terraform variables
-
 variable "environment" {
-  description = "Deployment environment (dev/test/prod)"
   type        = string
-  default     = "dev"
+  description = "Environment name (dev, test)"
 }
 
 variable "location" {
-  description = "Azure region for resources"
   type        = string
-  default     = "centralus"
-}
-
-
-variable "resource_group_name" {
-  description = "Resource group name"
-  type        = string
-  default     = "example-rg"
-}
-
-variable "vnet_name" {
-  description = "Virtual Network name"
-  type        = string
-  default     = "example-vnet"
-}
-
-variable "vnet_address_space" {
-  description = "VNet address space"
-  type        = list(string)
-  default     = ["10.0.0.0/16"]
-}
-
-variable "subnet_name" {
-  description = "Subnet name"
-  type        = string
-  default     = "example-subnet"
-}
-
-variable "subnet_address_prefix" {
-  description = "Subnet address prefix"
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-}
-
-variable "nic_name" {
-  description = "Network Interface name"
-  type        = string
-  default     = "example-nic"
-}
-
-variable "vm_name" {
-  description = "Virtual Machine name"
-  type        = string
-  default     = "example-vm"
+  default     = "eastus2"
 }
 
 variable "vm_size" {
-  description = "VM size"
   type        = string
   default     = "Standard_B1ms"
 }
 
 variable "admin_username" {
-  description = "Admin username for VM"
   type        = string
   default     = "azureuser"
 }
 
 variable "ssh_public_key" {
-  description = "SSH public key for VM access"
   type        = string
+  description = "SSH public key"
 }
 
-variable "os_publisher" {
-  description = "OS image publisher"
-  type        = string
-  default     = "Canonical"
+variable "vnet_address_space" {
+  type    = string
+  default = "10.0.0.0/16"
 }
 
-variable "os_offer" {
-  description = "OS image offer"
-  type        = string
-  default     = "UbuntuServer"
-}
-
-variable "os_sku" {
-  description = "OS image SKU"
-  type        = string
-  default     = "18.04-LTS"
-}
-
-variable "os_version" {
-  description = "OS image version"
-  type        = string
-  default     = "latest"
+variable "subnet_address_prefix" {
+  type    = string
+  default = "10.0.1.0/24"
 }
