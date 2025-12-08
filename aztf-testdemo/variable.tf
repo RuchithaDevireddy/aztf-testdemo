5713f4751fe92474e9173efc0a20e09ca846d8ad
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-08 16:43:48 +0000

    Update variable.tf

--- aztf-testdemo/variable.tf
+++ aztf-testdemo/variable.tf
@@ -1,34 +1,12 @@
+############################################
+# VARIABLES
+############################################
 variable "environment" {
   type        = string
   description = "Environment name (dev, test)"
 }
-
-variable "location" {
-  type        = string
-  default     = "eastus2"
-}
-
-variable "vm_size" {
-  type        = string
-  default     = "Standard_B1ms"
-}
-
-variable "admin_username" {
-  type        = string
-  default     = "azureuser"
-}
-
-variable "ssh_public_key" {
-  type        = string
-  description = "SSH public key"
-}
-
-variable "vnet_address_space" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_address_prefix" {
-  type    = string
-  default = "10.0.1.0/24"
-}
+variable "resource_group_name" {}
+variable "location" {}
+variable "vm_name" { default = "myvm" }
+variable "admin_username" { default = "azureuser" }
+variable "vm_size" { default = "Standard_B1s" }
