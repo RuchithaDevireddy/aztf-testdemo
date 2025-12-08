dee5d8bebef985d8db32efa94a68d869c75106c4
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-08 16:34:01 +0000

    Update outputs.tf

--- aztf-testdemo/outputs.tf
+++ aztf-testdemo/outputs.tf
@@ -1,3 +1,13 @@
+############################################
+# OUTPUTS
+############################################
+
+
+output "private_key_path" {
+value = local_file.private_key.filename
+}
+
+
 output "public_ip" {
-  value = azurerm_public_ip.public_ip.ip_address
+value = azurerm_public_ip.public_ip.ip_address
 }
