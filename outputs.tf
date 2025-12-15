b20e1fdc472f4885b4d94ae9e6922b95eb1931fa
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-15 22:25:51 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,11 @@
-############################################
-# OUTPUTS
-############################################
-
-
-output "private_key_path" {
-value = local_file.private_key.filename
+output "vm_public_ip" {
+  value       = azurerm_public_ip.public_ip.ip_address
+  description = "Public IP of the VM"
 }
 
-
-output "public_ip" {
-value = azurerm_public_ip.public_ip.ip_address
+output "vm_ssh_private_key" {
+  value       = tls_private_key.vm_key.private_key_pem
+  sensitive   = true
+  description = "Private key for SSH access"
 }
+
