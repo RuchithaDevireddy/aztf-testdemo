b1c53978ce18bb32f4d6b56494ab04182a4594c4
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-15 22:06:15 +0000

    Update main.tf

--- aztf-testdemo/main.tf
+++ aztf-testdemo/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 name = "dotnet-vm"
 resource_group_name = azurerm_resource_group.rg.name
 location = azurerm_resource_group.rg.location
-size = "Standard_B2s"
+size = "Standard_B1ms"
 admin_username = var.admin_username
 
 
