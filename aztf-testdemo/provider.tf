f18f6588706aab0d12c0031bfdf3290698b9c695
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-12-15 22:13:14 +0000

    Update provider.tf

--- aztf-testdemo/provider.tf
+++ aztf-testdemo/provider.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 3.90.0"   # use latest stable (currently 3.99+)
     }
   }
+  required_version = ">= 1.6.0"
 }
 
 provider "azurerm" {
